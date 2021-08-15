38401d79bce18af0ea0379729819dceab0f8b4b2
danial <lipsky@knu.ua>
2021-08-15 10:44:42 +0300

    make extra funcs to simlify

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ let form = document.forms.mainForm;
 
 const ESV = 1320;
 const MIN_SALARY = 6000;
+const EXTRA_EXPENSE = 100;
+// extraExpense
 
-document.getElementById('esv').innerHTML = ESV;
 form.newInCome.oninput = calcTaxes;
 
 countFivePerCentTaxes = (total) => (total*5)/100;
+
+showWarningMassage = () => document.getElementById('warningInfo').innerHTML = "Зашла сумма меньше минималки";
+
+hideWarningMassage = () => document.getElementById('warningInfo').innerHTML = "";
 
 function calcTaxes(){
     let newInCome = +form.newInCome.value;
@@ -14,17 +19,19 @@ function calcTaxes(){
     if(newInCome === 0 || !newInCome)  return;
 
     if(newInCome < MIN_SALARY && newInCome > 0){
-        document.getElementById('warningInfo').innerHTML = "Зашла сумма меньше минималки";
+        showWarningMassage();
         return;
     }
 
-    document.getElementById('warningInfo').innerHTML = "";
+    hideWarningMassage();
     
     let tax_five = countFivePerCentTaxes(newInCome);
     let clearMoney = newInCome - countFivePerCentTaxes(newInCome) - ESV;
     
     document.getElementById('tax_five').innerHTML = tax_five;
+    document.getElementById('esv').innerHTML = ESV;
     document.getElementById('clearMoney').innerHTML = clearMoney;
+    document.getElementById('extraExpense').innerHTML = EXTRA_EXPENSE;
 }
 
 calcTaxes();

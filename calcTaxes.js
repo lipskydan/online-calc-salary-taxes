a28716a7c3aa93447988dfa1730bc47e931a1114
Lipskyi.Danyil <lipsky@knu.ua>
2021-08-18 17:59:56 +0300

    work with modules

--- calcTaxes.js
+++ calcTaxes.js
@@ -1,40 +1,35 @@
-import {ESV, MIN_SALARY, EXTRA_EXPENSE} from './staticData.js'
-import {showWarningMassage, hideWarningMassage} from './formAnimation.js'
+import {ESV as esv, MIN_SALARY as m_s, EXTRA_EXPENSE as e_e} from './modules/staticData.mjs'
+
 class calcTaxes {
 
-    static ESV = () => this.ESV;
-    static MIN_SALARY = () => this.MIN_SALARY;
-    static EXTRA_EXPENSE = () => this.EXTRA_EXPENSE;
+    static ESV = () => esv;
+    static MIN_SALARY = () => m_s;
+    static EXTRA_EXPENSE = () => e_e;
 
-    constructor(form){
-        // this.inCome = inCome;
-        this.inCome = +form.newInCome.value;
+    constructor(newInCome){
+        // this.inCome = +form.newInCome.value;
+        this.inCome = newInCome;
     }
 
     countFivePerCentTaxes = (total) => (total*5)/100;
 
-    
-
     run(){
-        // let newInCome = +form.newInCome.value;
+        if(this.inCome === 0 || !this.inCome)  return;
     
-        if(newInCome === 0 || !newInCome)  return;
-    
-        if(newInCome < MIN_SALARY && newInCome > 0){
-            showWarningMassage();
+        if(this.inCome < MIN_SALARY()){
             return;
         }
-    
-        hideWarningMassage();
         
-        let tax_five = countFivePerCentTaxes(newInCome);
-        let clearMoney = newInCome - countFivePerCentTaxes(newInCome) - ESV;
-        
-        document.getElementById('tax_five').innerHTML = tax_five;
-        document.getElementById('esv').innerHTML = ESV;
-        document.getElementById('clearMoney').innerHTML = clearMoney;
-        document.getElementById('extraExpense').innerHTML = EXTRA_EXPENSE;
+        this.tax_five = countFivePerCentTaxes(this.inCome);
+        this.clearMoney = this.inCome - countFivePerCentTaxes(this.inCome) - ESV();
     }
 
-
+    info(){
+        console.log(this.inCome + " " + this.tax_five + " " + this.clearMoney);
+    }
 }
+
+
+let a = new calcTaxes(10000);
+a.run;
+a.info;

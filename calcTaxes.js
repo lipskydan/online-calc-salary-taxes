780154da7364cddf7b53eeb291e9561a9d115f70
Lipskyi.Danyil <lipsky@knu.ua>
2021-08-18 16:43:41 +0300

    use main calc func in class from app.js

--- calcTaxes.js
+++ calcTaxes.js
@@ -10,5 +10,26 @@ class calcTaxes {
         this.inCome = inCome;
     }
 
+    run(){
+        let newInCome = +form.newInCome.value;
+    
+        if(newInCome === 0 || !newInCome)  return;
+    
+        if(newInCome < MIN_SALARY && newInCome > 0){
+            showWarningMassage();
+            return;
+        }
+    
+        hideWarningMassage();
+        
+        let tax_five = countFivePerCentTaxes(newInCome);
+        let clearMoney = newInCome - countFivePerCentTaxes(newInCome) - ESV;
+        
+        document.getElementById('tax_five').innerHTML = tax_five;
+        document.getElementById('esv').innerHTML = ESV;
+        document.getElementById('clearMoney').innerHTML = clearMoney;
+        document.getElementById('extraExpense').innerHTML = EXTRA_EXPENSE;
+    }
+
 
 }
